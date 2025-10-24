82e88f99aacf9ea848db9944fc68127968f2a1f9
yogeshcoded <yogeshbyjava@gmail.com>
2025-10-24 23:57:22 +0530

    new changes update

--- read_private_data.py
+++ read_private_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests
 
-url="https://api.themoviedb.org/3/trending/all"
+url="https://api.themoviedb.org/3/trending/movie"
 headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYTgyOGJmYWQ4ZGZiYTM1MWUyOTgyOWUyOTFhZWQ2MiIsIm5iZiI6MTY5MTQ2NjExMS4zNDgsInN1YiI6IjY0ZDFiOTdmNmQ0Yzk3MDBlYzU4ZTRiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ptdKEYNeZGI2o8wzhlBesFF5hEdDSS5s7J92pegqX6I"
 }
